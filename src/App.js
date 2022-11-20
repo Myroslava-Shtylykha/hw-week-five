62d1ab61b4bff2e8daa8f298c29bbc8fab02549f
Myroslava-Shtylykha <mirunja@gmail.com>
2022-11-20 20:15:14 +0200

    Added link to GitHub

--- src/App.js
+++ src/App.js
@@ -4,8 +4,29 @@ import Weather from "./Weather";
 
 export default function App() {
     return (
-      <div className="container">
-        <Weather defaultCity="Lisbon" />
+      <div className="App">
+        <div className="container">
+          <Weather defaultCity="Lisbon" />
+         
+          <div className="link">
+            This project was coded by Myroslava Shtylykha and is{" "}
+            <a
+              href="https://github.com/Myroslava-Shtylykha/hw-week-five"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              open-sourced on GitHub
+            </a>{" "}
+            and{" "}
+            <a
+              href="https://courageous-shortbread-6472d5.netlify.app"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              hosted on Netlify
+            </a>
+          </div>
+        </div>
       </div>
     );
 }
