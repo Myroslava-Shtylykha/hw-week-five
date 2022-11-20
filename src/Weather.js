a1cf329096b8bd5e8defab15f139d1a7fe16582f
Myroslava-Shtylykha <mirunja@gmail.com>
2022-11-20 19:15:26 +0200

    Search Engine

--- src/Weather.js
+++ src/Weather.js
@@ -1,11 +1,12 @@
 import React, {useState} from "react";
-import CurrentDate from "./CurrentDate";
+import WeatherInfo from "./WeatherInfo";
 import "./Weather.css";
 import axios from "axios";
  
 export default function Weather(props) {
-  
   let [weatherData, setWeatherData] = useState({ready: false});
+  const [city, setCity] = useState(props.defaultCity);
+ 
   function handleResponse(response) {
     console.log(response.data);
     setWeatherData({
@@ -18,15 +19,32 @@ export default function Weather(props) {
       city: response.data.city,
       description: response.data.condition.description,
       iconUrl: response.data.condition.icon_url,
-      time: new Date(response.data.time * 1000)
+      time: new Date(response.data.time * 1000),
+      degree: response.data.wind.degree
     });
 
   }
 
+  function search() {
+const apiKey = `cct8eebao110f93a5ba96c40f6fbbab8`;
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+axios.get(apiUrl).then(handleResponse);
+
+  }
+
+function handleSubmit(event) {
+  event.preventDefault();
+  search();
+}
+
+function handleCityChange(event) {
+setCity(event.target.value);
+}
+
   if (weatherData.ready) {
     return (
       <div className="Weather">
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className="row">
             <div className="col-9">
               <input
@@ -34,6 +52,7 @@ export default function Weather(props) {
                 className="form-control"
                 placeholder="Enter a city"
                 autoFocus
+                onChange={handleCityChange}
               />
             </div>
             <div className="col-3">
@@ -41,44 +60,12 @@ export default function Weather(props) {
             </div>
           </div>
         </form>
-        <h1>{weatherData.city}</h1>
-        <div className="day_time">
-          <CurrentDate date={weatherData.time} />
-        </div>
-        <img src={weatherData.iconUrl} alt={weatherData.description}></img>
-        <ul>
-          <li className="temperature">
-            {Math.round(weatherData.temperature)}℃
-          </li>
-          <li className="text-capitalize">{weatherData.description}</li>
-          <li className="feels">
-            Feels like {Math.round(weatherData.feels)} ℃
-          </li>
-        </ul>
-        <hr />
-        <div className="footer">
-          <div className="row">
-            <div className="col-6">
-              <ul>
-                <li>Wind {weatherData.wind} m/s</li>
-                <li>Humidity {weatherData.humidity} %</li>
-              </ul>
-            </div>
-            <div className="col-6">
-              <ul>
-                <li>Precipitation 85 %</li>
-                <li>Pressure {weatherData.pressure} Pa</li>
-              </ul>
-            </div>
-          </div>
-        </div>
+        <WeatherInfo data={weatherData} />
+        
       </div>
     );
   } else {
-    const apiKey = `cct8eebao110f93a5ba96c40f6fbbab8`;
-    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${props.defaultCity}&key=${apiKey}&units=metric`;
-    axios.get(apiUrl).then(handleResponse);
-
+    search();
     return "Please, waight...";
   }
 }
---
+++ src/WeatherInfo.js
@@ -0,0 +1,36 @@
+import React from "react";
+import CurrentDate from "./CurrentDate";
+
+export default function WeatherInfo(props) {
+return (
+  <div className="WeatherInfo">
+    <h1> {props.data.city} </h1>
+    <div className="day_time">
+      <CurrentDate date={props.data.time} />
+    </div>
+    <img src={props.data.iconUrl} alt={props.data.description}></img>
+    <ul>
+      <li className="temperature">{Math.round(props.data.temperature)}℃</li>
+      <li className="text-capitalize">{props.data.description}</li>
+      <li className="feels">Feels like {Math.round(props.data.feels)} ℃</li>
+    </ul>
+    <hr />
+    <div className="footer">
+      <div className="row">
+        <div className="col-6">
+          <ul>
+            <li>Wind {props.data.wind} m/s</li>
+            <li>Humidity {props.data.humidity} %</li>
+          </ul>
+        </div>
+        <div className="col-6">
+          <ul>
+            <li>Wind degree {props.data.degree} °</li>
+            <li>Pressure {props.data.pressure} Pa</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+);
+}
